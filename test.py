6b08a8e3c35d2fdd9a250a8f60ddb1bf4c9e2e4d
Soliman <mohamed.h.hamed@gmail.com>
2023-10-14 23:03:25 +0300

    code and keywords tested successfully

--- test.py
+++ test.py
@@ -1,88 +1,96 @@
 import pandas as pd
+import re
 
-# Sample data
-data = {'column_A': ['Hello', 'مرحبا', 'Python', 'برمجة', "مصر"]}
+df = pd.read_csv('keywords.csv')
 
-# Create a DataFrame
-df = pd.DataFrame(data)
-
-# Function to replace the middle letter
 def replace_middle_letter(word):
-    equivalent_letters = {'ا': 'a', 
-                          'ب': 'b', 
-                          'ت': 't',
-                          'ث' :'th',
-                          'ج': 'g',
-                          'ح': 'h',
-                          'خ':'kh',
-                          'د': 'd',
-                          'ذ': 'z',
-                          'ر': 'r', 
-                          'ز': 'z',
-                          'س': 's',
-                          'ش' :'sh',
-                          'ص':'s',
-                          'ض':'d',
-                          'ط':'t',
-                          'ظ':'z',
-                          'ع':'a',
-                          'غ':'gh',
-                          'ف': 'f', 
-                          'ق': 'q', 
-                          'ك': 'k', 
-                          'ل': 'l', 
-                          'م': 'm', 
-                          'ن': 'n',
-                          'ه':'h', 
-                          'و': 'o',
-                          'ي': 'e', 
-                          'ى': 'e', 
-                          'a': 'ا', 
-                          'b': 'ب',
-                          'c':'س',
-                          'd': 'د', 
-                          'e': 'ي', 
-                          'f': 'ف', 
-                          'g': 'ج', 
-                          'h': 'ح',
-                          'i': 'ي', 
-                          'j': 'ج', 
-                          'k': 'ك', 
-                          'l': 'ل', 
-                          'm': 'م', 
-                          'n': 'ن', 
-                          'o': 'و', 
-                          'p': 'ب',
-                          'q': 'ق', 
-                          'r': 'ر', 
-                          's': 'س', 
-                          't': 'ت', 
-                          'u': 'و', 
-                          'v': 'ف', 
-                          'w': 'و', 
-                          'x': 'ك',
-                          'y': 'ي', 
-                          'z': 'ز'}
+    ''' 
+        # Function to replace the middle letter
+    '''
+    mapping = {'ا': 'a', 
+                'ب': 'b', 
+                'ت': 't',
+                'ث' :'th',
+                'ج': 'g',
+                'ح': 'h',
+                'خ':'kh',
+                'د': 'd',
+                'ذ': 'z',
+                'ر': 'r', 
+                'ز': 'z',
+                'س': 's',
+                'ش' :'sh',
+                'ص':'s',
+                'ض':'d',
+                'ط':'t',
+                'ظ':'z',
+                'ع':'a',
+                'غ':'gh',
+                'ف': 'f', 
+                'ق': 'q', 
+                'ك': 'k', 
+                'ل': 'l', 
+                'م': 'm', 
+                'ن': 'n',
+                'ه':'h', 
+                'و': 'o',
+                'ي': 'e', 
+                'ى': 'e',
+                'ئ':'e', 
+                'a': 'ا', 
+                'b': 'ب',
+                'c':'س',
+                'd': 'د', 
+                'e': 'ي', 
+                'f': 'ف', 
+                'g': 'ج', 
+                'h': 'ح',
+                'i': 'ي', 
+                'j': 'ج', 
+                'k': 'ك', 
+                'l': 'ل', 
+                'm': 'م', 
+                'n': 'ن', 
+                'o': 'و', 
+                'p': 'ب',
+                'q': 'ق', 
+                'r': 'ر', 
+                's': 'س', 
+                't': 'ت', 
+                'u': 'و', 
+                'v': 'ف', 
+                'w': 'و', 
+                'x': 'ك',
+                'y': 'ي', 
+                'z': 'ز'}
 
-
-    # if len(word) < 3:
-    #     return word  # If the word has less than 3 characters, return the original word
+    if len(word) < 3:
+        return word
 
     middle_index = len(word) // 2
     before_middle = word[:middle_index]
     after_middle = word[middle_index + 1:]
 
-    if word[middle_index] in equivalent_letters:
-        middle_letter = equivalent_letters[word[middle_index]]
+    if word[middle_index] in mapping:
+        middle_letter = mapping[word[middle_index]]
     else:
         middle_letter = word[middle_index]
 
-    return before_middle +middle_letter+after_middle
+    return  before_middle + "" + middle_letter + '.' + after_middle 
 
 # Replace middle letter in column_A with Arabic equivalents
-df['column_A'] = df['column_A'].apply(lambda word: replace_middle_letter(word))
- 
-# df['column_A'] = df['column_A'].apply(lambda x: x[::-1])
+# df['column_b'] = df['column_A'].apply(lambda word: replace_middle_letter(word))
 
+with open("input.txt", "r", encoding="utf-8") as f:
+    input_text = f.read()
 
-print(df)
+words = re.findall(r'\b\w+\b', input_text)
+
+# Apply the function to each word and reconstruct the modified text
+modified_words = [replace_middle_letter(word) if word in df['column_A'].values else word for word in words]
+modified_text = " ".join(modified_words)
+
+file_path = "output.txt"
+# Open the file in write mode and write the string to it
+with open(file_path, "w", encoding="utf-8") as file:
+    file.write(modified_text)
